9d68888b2bcdd23b6c8ec898fd5fccc9a3796897
voidspacexyz <null@voidspace.xyz>
2018-01-15 21:26:44 +0530

    Migrations for PHR and HMS

--- mrms/mrms/settings.py
+++ mrms/mrms/settings.py
@@ -151,9 +151,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 APPEND_SLASH = True
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.zoho.com'
+EMAIL_HOST = 'smtp.live.com'
 EMAIL_PORT = 25
-EMAIL_HOST_USER = 'Username@atom360.io'
-EMAIL_HOST_PASSWORD = 'enter_password_here'
-DEFAULT_FROM_EMAIL = 'Username@atom360.io'
+EMAIL_HOST_USER = 'me_the_3d@hotmail.com'
+EMAIL_HOST_PASSWORD = 'iamthe3dimension'
+DEFAULT_FROM_EMAIL = 'me_the_3d@hotmail.com'
 DEFAULT_TO_EMAIL = 'to email'
